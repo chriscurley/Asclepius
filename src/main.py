531867c3962372b7d7b488fe2794acfb3e4f9893
chriscurley <curley.christian.b@gmail.com>
2018-07-21 20:46:27 -0600

    Updates to main.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Project Asclepius
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import test_train_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score,confusion_matrix
